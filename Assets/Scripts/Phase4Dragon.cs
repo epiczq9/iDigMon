96f0f4daf857f97dbdce29480c532f650766e17a
nikolaknezevic <nikola.knezevic97@gmail.com>
2022-11-09 14:35:36 +0100

    paint ubacen

--- Assets/Scripts/Phase4Dragon.cs
+++ Assets/Scripts/Phase4Dragon.cs
@@ -44,10 +44,6 @@ public class Phase4Dragon : MonoBehaviour
             } else if (slime2CanBeMelted) {
                 MeltTheSlime2();
             }
-
-            if (!bagOpened && bagCanBeOpened) {
-                OpenBag();
-            }
             swipeDet.outputText.text = "EMPTY";
         } else if (swipeDet.outputText.text == "DOWN" || Input.GetKeyDown(KeyCode.DownArrow)) {
             
@@ -56,7 +52,8 @@ public class Phase4Dragon : MonoBehaviour
     }
 
     public void MeltTheSlime1() {
-        if(swipeCounter1 == 0) {
+        doll.SetActive(true);
+        if (swipeCounter1 == 0) {
             slimeFull.SetActive(false);
             slimeHalfed.SetActive(true);
             slimeHalf1.GetComponent<MeltSlime>().doFirstSwipe = true;
@@ -81,65 +78,22 @@ public class Phase4Dragon : MonoBehaviour
             uiLRLower.SetActive(false);
             dollPullOutButton.SetActive(true);
         }
-    }
-
-    
+    } 
     public void PullOutDoll() {
-        //slimeFull.SetActive(false);
-        //slimeHalfed.SetActive(true);
         doll.transform.parent = null;
-        //slimeHalf1.transform.parent = null;
-        //slimeHalf2.transform.parent = null;
         SwitchCamsForDollPulling();
         doll.transform.DOMove(dollPulledTran.position, 0.75f);
         doll.transform.DORotate(dollPulledTran.eulerAngles, 0.75f).OnComplete(SetAsideSlime);
-        
-        /*
-        slimeHalf1.transform.DOMove(slimeHalfTran1.position, 0.75f);
-        slimeHalf1.transform.DORotate(slimeHalfTran1.eulerAngles, 0.75f);
-        slimeHalf2.transform.DOMove(slimeHalfTran2.position, 0.75f);
-        slimeHalf2.transform.DORotate(slimeHalfTran2.eulerAngles, 0.75f).OnComplete(PositionBagForOpening);
-        */
     }
-    public void SetAsideSlime() {
-        slimeHalfed.transform.DOMove(slimeSetAsideTran.position, 0.75f);
-    }
-    /*
-    public void PositionBagForOpening() {
-        SwitchCamsForBagOpening();
-        doll.transform.DOMove(bagOpeningTran.position, 0.75f);
-        doll.transform.DORotate(bagOpeningTran.eulerAngles, 0.75f).OnComplete(EnableBagOpening);
-    }
-    */
     public void SwitchCamsForDollPulling() {
         vCamStart.Priority = 10;
         vCamDollPulling.Priority = 20;
     }
-    public void EnableBagOpening() {
-        bagCanBeOpened = true;
-        uiSwipeRight.SetActive(true);
-        //doll.transform.position = dollInBagTran.position;
-        //doll.transform.rotation = dollInBagTran.rotation;
+    public void SetAsideSlime() {
+        slimeHalfed.transform.DOMove(slimeSetAsideTran.position, 0.75f).OnComplete(ActivatePhase5);
     }
-    public void OpenBag() {
-        doll.GetComponent<Animator>().Play("OpenBag");
-        TimersManager.SetTimer(this, 0.5f, EnableDollTakeOut);
-    }
-    public void EnableDollTakeOut() {
-        bagOpened = true;
-        uiSwipeUp.SetActive(true);
-    }
-    public void BagSetAside() {
-        SwitchCamsToDollZoomIn();
-        //bag.transform.DOMove(bagSetAsideTran.position, 0.75f).OnComplete(ActivatePhase5);
-    }
-    public void SwitchCamsToDollZoomIn() {
-        vCamDollPulling.Priority = 10;
-        vCamDollZoomIn.Priority = 20;
-    }
+    
     public void ActivatePhase5() {
-        //doll2.transform.position = dollPullOutTran.position;
-        //doll1.SetActive(false);
         gameObject.SetActive(false);
         phaseToActivate.SetActive(true);
     }
